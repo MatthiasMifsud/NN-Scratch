0b8e048455bc8dee3e3f089f849e6894d273cf63
MatthiasMifsud <matthiasmifsud3@gmail.com>
2025-06-30 16:48:44 +0200

    getting_data

--- C/main.c
+++ C/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
+
 
 #define INPUT_SIZE 28*28
 #define OUTPUT_SIZE 10
 #define HIDDEN_LAYER_SIZE 16
 
+#define TEST_PATH "./data/mnist_test.csv"
+#define TRAIN_PATH "./data/mnist_train.csv"
+#define MAX_LINE_LENGTH 2000
+
 struct Parameters{
-    double *weights_1;
-    double *bias_1;
-    double *weights_2;
-    double *bias_2;
+    double *W_1;
+    double *B_1;
+    double *W_2;
+    double *B_2;
 } param;
 
 double randn(){
@@ -21,32 +27,35 @@ double randn(){
 }
 
 void init_param(){
-    param.weights_1 = malloc(HIDDEN_LAYER_SIZE * INPUT_SIZE * sizeof(double));
-    param.bias_1 = malloc(HIDDEN_LAYER_SIZE * sizeof(double));
-    param.weights_2 = malloc(OUTPUT_SIZE * HIDDEN_LAYER_SIZE * sizeof(double));
-    param.bias_2 = malloc(OUTPUT_SIZE * sizeof(double));
+    param.W_1 = malloc(HIDDEN_LAYER_SIZE * INPUT_SIZE * sizeof(double));
+    param.B_1 = malloc(HIDDEN_LAYER_SIZE * sizeof(double));
+    param.W_2 = malloc(OUTPUT_SIZE * HIDDEN_LAYER_SIZE * sizeof(double));
+    param.B_2 = malloc(OUTPUT_SIZE * sizeof(double));
 
     for (int i = 0; i < HIDDEN_LAYER_SIZE * INPUT_SIZE; i++)
     {
-        param.weights_1[i] = randn();
+        param.W_1[i] = randn();
 
         if (i <= HIDDEN_LAYER_SIZE) 
-            param.bias_1[i] = randn();
+            param.B_1[i] = randn();
     }
 
     for (int i = 0; i < OUTPUT_SIZE * HIDDEN_LAYER_SIZE; i++)
     {
-        param.weights_2[i] = randn();
+        param.W_2[i] = randn();
 
         if (i <= OUTPUT_SIZE)
-            param.bias_2[i] = randn();
+            param.B_2[i] = randn();
     }
 }
+
+// activation functions 
 
 double ReLU(double x){
     return (x > 0.0) ? x : 0.0;
 }
 
+// function to find maximum of an array
 double maximum(double *array, const int size){
     double max = array[0];
 
@@ -77,7 +86,16 @@ double *Softmax(double *input, const int size){
     return input;
 }
 
-double *forward_prop(double input[INPUT_SIZE]){
+double ReLU_deriv(double x){
+
+}
+
+double Softmax_deriv(double x){
+
+}
+
+// aim is to get the output Y from an input X
+double *forward_prop(double X[INPUT_SIZE]){
     double weighted_sum = 0.0;
     
     // first part
@@ -86,10 +104,10 @@ double *forward_prop(double input[INPUT_SIZE]){
 
     for (int i = 0; i < HIDDEN_LAYER_SIZE; i++)
     {
-        double sum = param.bias_1[i];
+        double sum = param.B_1[i];
         for (int j = 0; j < INPUT_SIZE; j++)
         {
-            sum += input[j] * param.weights_1[i * INPUT_SIZE + j];
+            sum += X[j] * param.W_1[i * INPUT_SIZE + j];
 
         }
         activation_1[i] = ReLU(sum);
@@ -98,10 +116,10 @@ double *forward_prop(double input[INPUT_SIZE]){
 
     for (int i = 0; i < OUTPUT_SIZE; i++)
     {
-        double sum = param.bias_1[i];
+        double sum = param.B_2[i];
         for (int j = 0; j < HIDDEN_LAYER_SIZE; j++)
         {
-            sum += input[j] * param.weights_2[i * HIDDEN_LAYER_SIZE + j];
+            sum += X[j] * param.W_2[i * HIDDEN_LAYER_SIZE + j];
         }
         activation_2[i] = sum;
     }
@@ -113,15 +131,68 @@ double *forward_prop(double input[INPUT_SIZE]){
     return activation_2;
 }
 
-double **backward_prop(){
+double *backward_prop(double Y[OUTPUT_SIZE]){
+    
+}
+int file_size(FILE* file, char *line){
+    int count = 0;
+    char c;
 
+    //skipping the first line (header)
+    fgets(line, sizeof(line), file);
+
+    //reading one line at a time
+    while ((c = fgetc(file)) != EOF){
+        if (c == '\n') count++;
+    }
+
+    return count-1; //decrementing count by 1 to skip header
 }
 
 
+void read_data(){
+    FILE* test_data = fopen(TEST_PATH, "r");
+    FILE* train_data = fopen(TRAIN_PATH, "r");
+
+    if (!test_data || !train_data){
+        perror("ERROR: Cant find/open file!\n");
+        exit(1);
+    }
+
+    char line[MAX_LINE_LENGTH];
+    const int train_size = file_size(train_data, line);
+    const int test_size = file_size(test_data, line);
+    int y_train[train_size];
+    int y_test[test_size];
+    double pixels[INPUT_SIZE];
+    int pos = 0;
+
+
+    //reading the test data
+    //skipping the first line (header)
+    fgets(line, sizeof(line), test_data);
+
+    //reading one line at a time
+    while (fgets(line, sizeof(line), test_data)){
+        //tokenising each character in the line and separating labels and pixels
+        char *token = strtok(line, ",");
+        y_test[pos] = atoi(token); //pointer to the first token is the label
+        pos++;
+
+        for (int i = 0; i < INPUT_SIZE; i++)
+        {
+            token = strtok(NULL, ",");
+
+            if (token != NULL)
+                pixels[i] = atof(token);
+            else
+                perror("ERROR: Incomplete pixel data");
+        }
+    }
+}
+
 
 int main(void){
-    init_param();
-
-
+    read_data();
     return 0;
 }
